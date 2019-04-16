76c94bed6554154e003e90926edce58df959b274
Volker Hartmann <volker.hartmann@.kit.edu>
2019-04-16 14:44:06 +0200

    Travis: Build KIT DataManager in beforehand.

--- build-with-dependencies.sh
+++ build-with-dependencies.sh
@@ -3,6 +3,19 @@
 #   No dependencies yet
 cp ./test/application.properties .
 
+# Build Repository
+git clone https://github.com/kit-data-manager/base-repo.git
+cd base-repo
+git submodule init
+git submodule update --remote --merge 
+cd libraries/service-base/
+./gradlew install
+cd ../../
+cd libraries/generic-message-consumer/
+./gradlew install
+cd ../../
+./gradlew -Prelease build
+
 # Create test report
 echo "Building project and executing tests"
 ./gradlew clean check jacocoTestReport
