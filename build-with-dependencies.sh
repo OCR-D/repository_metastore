64b0ba2de0ca025cc76c3b047bd59fbe38948616
Volker Hartmann <volker.hartmann@.kit.edu>
2019-05-08 07:13:07 +0200

    Fix bug in build script. (Build service-base)

--- build-with-dependencies.sh
+++ build-with-dependencies.sh
@@ -5,13 +5,18 @@ cp ./test/application.properties .
 
 # Build Repository
 git clone https://github.com/kit-data-manager/docker-kitdm-2.0.git
-cd docker-kitdm-2.0
-cd source
+cd docker-kitdm-2.0/source
 docker-compose up &
 cd ../..
 
 # Wait for setup of KIT DM and its database
 sleep 480
+
+# Clone base-repo
+git clone https://github.com/kit-data-manager/service-base.git
+cd service-base/
+./gradlew install
+cd ..
 
 # Create test report
 echo "Building project and executing tests"
