15e4d9aff28a3311d5c2a0ce916e23faab704b6f
Volker Hartmann <volker.hartmann@kit.edu>
2019-12-18 14:05:12 +0100

    Check for installed commmands before executing script.

--- installDocker/setupDocker.sh
+++ installDocker/setupDocker.sh
@@ -7,6 +7,18 @@
 # bash installRepo.sh /path/to/installationdir
 ################################################################################
 
+# Test for commands used in this script
+testForCommands="sed git java"
+
+for command in $testForCommands
+do 
+  $command --help >> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Error: command '$command' is not installed!"
+    exit 1
+  fi
+done
+
 # Check if argument is given
 if [ -z "$1" ]; then
   echo Please provide a directory where to install.
@@ -23,37 +35,38 @@ if [ ! -d "$INSTALLATION_DIRECTORY" ]; then
   mkdir -p "$INSTALLATION_DIRECTORY"
 fi
 
+cd "$INSTALLATION_DIRECTORY"
+INSTALLATION_DIRECTORY=`pwd`
+
 # Prepare docker volumes
 # Data folders
-mkdir -p /install/server/arangodb
-mkdir -p /install/server/postgres
-mkdir -p /install/server/kitdm
+mkdir -p "$INSTALLATION_DIRECTORY"/server/arangodb
+mkdir -p "$INSTALLATION_DIRECTORY"/server/postgres
+mkdir -p "$INSTALLATION_DIRECTORY"/server/kitdm
 # Backup folders
-mkdir -p /install/server/backup/arangodb
-mkdir -p /install/server/backup/postgres
+mkdir -p "$INSTALLATION_DIRECTORY"/server/backup/arangodb
+mkdir -p "$INSTALLATION_DIRECTORY"/server/backup/postgres
 
 # Create directory for git projects
-mkdir /install/git
+mkdir "$INSTALLATION_DIRECTORY"/git
 # Checkout and build KIT Data Manager 2.0
-cd /install/git
+cd "$INSTALLATION_DIRECTORY"/git
 git clone --recursive https://github.com/kit-data-manager/base-repo.git
 cd base-repo
 ./gradlew -Pclean-release build
-cp build/libs/*.jar /install/base-repo/
-cd ../..
+cp build/libs/*.jar "$INSTALLATION_DIRECTORY"/base-repo/
 
 # Checkout and build Metastore
-cd git
+cd "$INSTALLATION_DIRECTORY"/git
 git clone --recursive https://github.com/OCR-D/repository_metastore.git
 cd repository_metastore
 ./gradlew -Prelease build
-cp build/libs/*.jar /install/metastore/
-cd ../..
+cp build/libs/*.jar "$INSTALLATION_DIRECTORY"/metastore/
 
 # Remove git projects
-rm -rf git
+rm -rf "$INSTALLATION_DIRECTORY"/git
 
-chmod -R a+rwX /install
+chmod -R a+rwX "$INSTALLATION_DIRECTORY"
 
 # End of setup for docker compose
 
